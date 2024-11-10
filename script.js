920a97f889d5249d3245fff683e186be36120a8e
simsekelif <kahyaelif@gmail.com>
2024-11-10 23:46:51 +0100

    printed timer to the playground

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const timer = setInterval(() => {
   count--;
   // This line prints the count to the console!
   console.log(count);
+  // printed timer to the playground with this attribute.
+  document.getElementById("secondsLeft").innerText = count;
   if (count === 0) {
     clearInterval(timer);
     console.log("Time's up! Your score: ");
