9ff31d9d184105f68b01c598ffed29fed19547d8
simsekelif <kahyaelif@gmail.com>
2024-11-16 21:02:17 +0100

    added cards

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 // timer function
 
-let count = 60;
+//Do not forget to change the amount!
+let count = 5;
 const timer = setInterval(() => {
   count--;
   // This line prints the count to the console!
   console.log(count);
   // printed timer to the playground with this attribute.
   document.getElementById("secondsLeft").innerText = count;
+  
   if (count === 0) {
     clearInterval(timer);
+    document.getElementById("gameButton").disabled = true;
     console.log("Time's up! Your score: ");
   }
 }, 1000);
+
+
+// score function
+
+// let score = 0;
+// for(var score = 0; i < 20; i++) {
+
+// }
+// function addOn(){
+//   score++
+// };
+// console.log(addOn);
+
+
+let cards =[ 
+  firstCard = {
+  word: "Watt",
+  forbiddenWords: ["joule", "unit", "power","electric", "potential"]
+},
+  secondCard = {
+    word: "Telescope",
+    forbiddenWords: ["star", "observatory", "planet", "look", "astronomy"]
+  },
+  thirdCard = {
+    word: "Magma",
+    forbiddenWords: ["volcano", "fiery", "liquid", "rock", "lava"]
+  }
+]
