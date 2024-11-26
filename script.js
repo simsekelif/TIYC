fce61c52669ebe07c04512818366bb3357c5c8d3
simsekelif <kahyaelif@gmail.com>
2024-11-26 22:39:11 +0100

    updated score function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// timer function
 
+
+import JSON from './cards.json' with { type: "json" };
+
+// timer function
 //Do not forget to change the seconds as 60!
 let count = 10;
+
   // printed timer to the playground with this attribute.
   document.getElementById("secondsLeft").innerText = count;
  
@@ -9,62 +13,75 @@ const timer = setInterval(() => {
   count--;
   // need to update again.
   document.getElementById("secondsLeft").innerText = count;
-  // This line prints the count to the console!
   console.log(count);
   
   if (count === 0) {
     clearInterval(timer);
-    
+    document.getElementById("times_up").innerText = "Time's Up";
+    //alert("Times Up !!");
     const buttons = document.querySelectorAll("#buttons button");
     buttons.forEach((button) => {
       button.disabled = true;
 
-      console.log("Time's up! Your score: "); 
   });
   
-}
+  }
 }, 1000);
 
 //score function
 
-let score = -1 ;
-for(let i = 0; i < 50; i++) {
+let score = 0 ;
 
-}
 function addOn(){
   score++;
-  document.getElementById("showScore").innerText = score
+  updateScore();
 };
-console.log(score);
-addOn();
 
 //function for wrong button
 function subtractFrom() {
   if (score > 0){
     score--;
   }
-  document.getElementById("showScore").innerText = score
+  updateScore();
 };
-console.log(score);
-subtractFrom();
+
+// update function for adding and substracking from the score
+function updateScore (){
+  const scoreElement = document.getElementById("showScore");
+  if(scoreElement){
+    scoreElement.innerText=score;
+  } else {
+    console.error("There is something wrong");
+  }
+}
 
 
 //cards are below! with JSON
 let cards =[];
 let currentCardIndex = 0;
 
-fetch('cards.json')
-  .then((response)=> {
-    return response.json();
-  })
-  .then((data) => {
-    cards = data;
-    showCard(currentCardIndex);
-  })
-  .catch((error) => {
-    console.log('error',error);
-  })
+for (let index = 0; index < JSON.cards.length; index++) {
+  const element = JSON.cards[index];
+  cards.push(element);
+  
+}
+ console.log("Json Data",JSON);
 
+
+// fetch('./cards.json')
+//   .then((response)=> {
+//     console.log(response.json());
+//     return response.json();
+//   })
+//   .then((data) => {
+//     cards = data;
+//     showCard(currentCardIndex);
+//   })
+//   .catch((error) => {
+//     console.log('error',error);
+//   })
+
+  
 function showCard(cardsIndex){
   const card = cards[cardsIndex];
   const wordElement = document.getElementById("mainWord");
@@ -92,3 +109,20 @@ function getNewCard(){
   }
   showCard(currentCardIndex);
 }
+
+// let timerAlert = new Promise(function(resolve,reject){
+//   if (count === 0) {
+//     resolve ("time's up")
+//   } else {
+//     reject ('take your time!')
+//   }
+
+// })
+// timerAlert
+//   .then(function(fromResolve){
+//     console.log('Yay ' + fromResolve)
+//   }
+//   .catch(function(fromReject){
+//     console.log("take your time "+ fromReject)
+//   }
+// ))
