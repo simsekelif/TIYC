1ca47e0c3e1809b4f97e868387c2baab76797c97
simsekelif <kahyaelif@gmail.com>
2024-11-27 22:18:52 +0100

    category.html is deleted, indentation

--- script.js
+++ script.js
@@ -1,63 +1,57 @@
-
-
-import JSON from './cards.json' with { type: "json" };
+// import JSON from './cards.json' with { type: "json" };
 
 // timer function
 //Do not forget to change the seconds as 60!
 let count = 10;
 
-  // printed timer to the playground with this attribute.
-  document.getElementById("secondsLeft").innerText = count;
- 
+// printed timer to the playground with this attribute.
+document.getElementById("secondsLeft").innerText = count;
+
 const timer = setInterval(() => {
   count--;
   // need to update again.
   document.getElementById("secondsLeft").innerText = count;
-  console.log(count);
-  
+  // console.log(count);
+
   if (count === 0) {
     clearInterval(timer);
     document.getElementById("times_up").innerText = "Time's Up";
-    //alert("Times Up !!");
+    // alert("Times Up !!");
     const buttons = document.querySelectorAll("#buttons button");
     buttons.forEach((button) => {
       button.disabled = true;
-
-  });
-  
+    });
   }
 }, 1000);
 
-//score function
+// score function
 
-let score = 0 ;
+let score = 0;
 
-function addOn(){
+function addOn() {
   score++;
   updateScore();
-};
+}
 
-
-//function for wrong button
+// function for wrong button
 function subtractFrom() {
-  if (score > 0){
+  if (score > 0) {
     score--;
   }
   updateScore();
-};
+}
 
-
-// update function for adding and substracking from the score
-function updateScore (){
+// update function for adding and substracting from the score
+function updateScore() {
   const scoreElement = document.getElementById("showScore");
-  if(scoreElement){
-    scoreElement.innerText=score;
+  if (scoreElement) {
+    scoreElement.innerText = score;
   } else {
-    console.error("There is something wrong");
+    console.error("There is something wrong!");
   }
 }
 
-//event listeners for the buttons
+// event listeners for the buttons
 document.getElementById("nextButton").addEventListener("click", () => {
   addOn();
   getNewCard();
@@ -65,77 +59,73 @@ document.getElementById("nextButton").addEventListener("click", () => {
 document.getElementById("wrongButton").addEventListener("click", () => {
   subtractFrom();
   getNewCard();
-})
-document.getElementById("passButton").addEventListener("click",getNewCard);
+});
+document.getElementById("passButton").addEventListener("click", getNewCard);
 
-//cards are below! with JSON
-let cards =[];
+// cards are below! with JSON
+let cards = [];
 let currentCardIndex = 0;
 
-for (let index = 0; index < JSON.cards.length; index++) {
-  const element = JSON.cards[index];
-  cards.push(element);
-  
-}
- console.log("Json Data",JSON);
+fetch("./cards.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    cards = data.cards;
+    showCard(currentCardIndex);
+  })
+  .catch((error) => {
+    console.error("error", error);
+  });
 
+// for (let index = 0; index < JSON.cards.length; index++) {
+//   const element = JSON.cards[index];
+//   cards.push(element);
+// }
+// console.log({cards, JSON})
 
-// fetch('./cards.json')
-//   .then((response)=> {
-//     console.log(response.json());
-//     return response.json();
-//   })
-//   .then((data) => {
-//     cards = data;
-//     showCard(currentCardIndex);
-//   })
-//   .catch((error) => {
-//     console.log('error',error);
-//   })
+//  console.log("Json Data",JSON);
 
-  
-function showCard(cardsIndex){
+function showCard(cardsIndex) {
   const card = cards[cardsIndex];
   const wordElement = document.getElementById("mainWord");
   const forbiddenWordsElement = document.getElementById("forbiddenWords");
-//update main word
+  //update main word
   wordElement.innerText = card.word;
-//clean the forbidden words list
-  forbiddenWordsElement.innerHTML ="";
-//adds new words to the forbidden words list
- card.forbiddenWords.forEach((word) => {
-  const li = document.createElement("li");
-  li.innerText = word;
-  forbiddenWordsElement.appendChild(li);
 
- });
-
+  //clean the forbidden words list
+  forbiddenWordsElement.innerHTML = "";
+  //adds new words to the forbidden words list
+  card.forbiddenWords.forEach((word) => {
+    const li = document.createElement("li");
+    li.innerText = word;
+    forbiddenWordsElement.appendChild(li);
+  });
 }
-showCard(0);
+// showCard(0);
 
 // function for the buttons for a new card.
-function getNewCard(){
+function getNewCard() {
   currentCardIndex++;
-  if(currentCardIndex >= cards.length){
+  if (currentCardIndex >= cards.length) {
     currentCardIndex = 0;
   }
   showCard(currentCardIndex);
 }
-getNewCard();
 
 // let timerAlert = new Promise(function(resolve,reject){
+
 //   if (count === 0) {
-//     resolve ("time's up")
+//     resolve ("time's flying!")
 //   } else {
 //     reject ('take your time!')
 //   }
-
 // })
+
 // timerAlert
 //   .then(function(fromResolve){
-//     console.log('Yay ' + fromResolve)
-//   }
+//     alert('Sorry, ' + fromResolve)
+//   })
 //   .catch(function(fromReject){
-//     console.log("take your time "+ fromReject)
-//   }
-// ))
+//     alert("take your time "+ fromReject)
+//   })
